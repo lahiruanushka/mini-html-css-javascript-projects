d63cba4bcda06ee4f78d4f5be31f53c6b42c993b
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-09-10 18:41:37 +0530

    Design the UI

---
+++ 10.Anime Pics Generator/index.js
@@ -0,0 +1,14 @@
+const btnEl = document.getElementById("btn");
+const animeContainerEl = document.querySelector(".anime-container");
+const animeImaEl = document.querySelector(".anime-img");
+const animeNameEl = document.querySelector(".anime-name");
+
+const apiURL = "";
+
+btnEl.addEventListener("click", async () => {
+  try {
+    
+  } catch (error) {
+    console.error(error);
+  }
+});
